d6a32a82f63a6af1c263d11f3631210f2ccf0bbf
YeomanYe <ming13821007683@gmail.com>
2017-05-10 23:21:51 +0800

    add localStorage to pinBoundle

--- js/emulator.js
+++ js/emulator.js
@@ -1,7 +1,24 @@
 window.onload = function () {
+    var i,j,itemName,itemValue;
     var pins = document.querySelectorAll(".pin");
-    for(var i=0,len=pins.length;i<len;i++){
-        pins[i].addEventListener('change',portChangeHandler,false);
+    var pinChangeHandler =function(evt){
+            var elem = evt.currentTarget;
+            pinBound(elem);
+    };
+    for(i=0,len=pins.length;i<len;i++){
+        pins[i].addEventListener('change',pinChangeHandler,false);
+    }
+    for(i=0;i<4;i++){
+        itemName = "P"+ i + "_";
+        for(j=0;j<8;j++){
+            itemName += j;
+            itemValue = localStorage.getItem(itemName);
+            if(itemValue){
+                var item = document.getElementById(itemName);
+                item.value = itemValue;
+                pinBound(item);
+            }
+        }
     }
     canvasInit();
     draw();
@@ -12,13 +29,13 @@ var digA,digB,digC,digD,digE,digF,digG,digDP;
 var dig1,dig2,dig3,dig4,dig5,dig6,dig7,dig8;
 //端口改变处理函数
 
-function portChangeHandler(evt){
-    var portElem = evt.currentTarget,
-        portId = portElem.id,
+function pinBound(portElem){
+    var portId = portElem.id,
         portValue = portElem.value,
         expression = /P([0-9])_([0-9])/,
         portNum = expression.exec(portId)[1],
         pinNum = expression.exec(portId)[2];
+    localStorage.setItem(portId,portValue);
     var temp = function(){return STC90C51.getSFRsBit(0x80+portNum*16+pinNum);};
     switch(portValue.toUpperCase()){
         case "A":digA=temp;break;
